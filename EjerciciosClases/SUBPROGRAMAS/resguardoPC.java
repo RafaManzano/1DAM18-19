18d0c16169298e7485a7e24b6869b764203c3cf9
RafaManzano <rafamanzanomedina@gmail.com>
2018-11-20 10:06:31 +0100

    Subiendo archivos

---
+++ EjerciciosClases/SUBPROGRAMAS/resguardoPC.java
@@ -0,0 +1,53 @@
+/*
+ * resguardoPC
+ * 
+ */
+
+
+public class resguardoPC {
+	
+	/*
+	 * Nec: No hay
+	 * Dev: No hay
+	 * Nec/Dev: No hay
+	 * Requisitos: No hay
+	 * 
+	 * Interfaz
+	 * Nombre: PresentarMenu
+	 * Comentario: Muestra el menu principal
+	 * Cabecera: void PresentarMenu();
+	 * Precondiciones: No hay
+	 * Entrada: No hay
+	 * Salida: No hay
+	 * E/S: No hay
+	 * Postcondiciones: Solo pinta en pantalla
+	 * 
+	*/
+	public static void PresentarMenu() {
+		System.out.println("En resguardo");
+	}
+	
+	/*
+	 * Nec: 1 numero
+	 * Dev: 1 numero
+	 * Nec/Dev: No hay
+	 * Requisitos: No hay
+	 * 
+	 * Interfaz
+	 * Nombre: CalcularFactorial
+	 * Comentario: Calcular un numero factorial
+	 * Cabecera: int calcularFactorial (int numero)
+	 * Precondiciones: No hay
+	 * Entrada: 1 int 
+	 * Salida: 1 int 
+	 * E/S: No hay
+	 * Postcondiciones: Asociado al nombre el resultado del factorial
+	 *
+	*/
+	
+	public static int calcularFactorial (int numero) {
+		System.out.println("En resguardo");
+		return 1;
+	}
+}
+
---
+++ EjerciciosClases/SUBPROGRAMAS/utilidadPC.java
@@ -0,0 +1,57 @@
+/*
+ * 
+ * 
+ */
+
+
+public class utilidadPC {
+	
+	/*
+	 * Nec: No hay
+	 * Dev: No hay
+	 * Nec/Dev: No hay
+	 * Requisitos: No hay
+	 * 
+	 * Interfaz
+	 * Nombre: PresentarMenu
+	 * Comentario: Muestra el menu principal
+	 * Cabecera: void PresentarMenu();
+	 * Precondiciones: No hay
+	 * Entrada: No hay
+	 * Salida: No hay
+	 * E/S: No hay
+	 * Postcondiciones: Solo pinta en pantalla
+	 * 
+	*/
+	
+	public static void PresentarMenu() {
+		System.out.println("Elija una opcion");
+		System.out.println("1. Piramide de Tartaglia");
+		System.out.println("2. Conjetura de Collatz");
+		System.out.println("0. Salir");
+	}
+	
+	/*
+	 * Nec: 1 numero
+	 * Dev: 1 numero
+	 * Nec/Dev: No hay
+	 * Requisitos: No hay
+	 * 
+	 * Interfaz
+	 * Nombre: CalcularFactorial
+	 * Comentario: Calcular un numero factorial
+	 * Cabecera: int calcularFactorial (int numero)
+	 * Precondiciones: No hay
+	 * Entrada: 1 int 
+	 * Salida: 1 int 
+	 * E/S: No hay
+	 * Postcondiciones: Asociado al nombre el resultado del factorial
+	 *
+	*/
+	
+	//public static int calcularFactorial(int numero) {
+			
+		//}
+	}
+
+
