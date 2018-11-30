08042868447529b9965fbb4d04aa14ddab8c1d8b
RafaManzano <rafamanzanomedina@gmail.com>
2018-11-30 20:34:09 +0100

    Subiendo funcionalidades de recursividad

--- EjerciciosClases/SUBPROGRAMAS/DIVIENDOYMULTIPLICANDO/FuncionalidadesDM.java
+++ EjerciciosClases/SUBPROGRAMAS/DIVIENDOYMULTIPLICANDO/FuncionalidadesDM.java
@@ -8,21 +8,34 @@
 
 public class FuncionalidadesDM {
 	
-	/*
-	 * Nec: Dos numeros
-	 * Dev: Dos resultados
+/*
+	 * Nec: Dos numeros //Dividendo y divisor, Por valor
+	 * Dev: Dos numero //Cociente y resto //Por Referencia
 	 * Nec/Dev: No hay
-	 * Requisitos: No hay
+	 * Requisitos: Ambos numeros deben ser mayores que 0
 	 * 
-	 * Interfaz
+	 * Interfaz intermedia
+	 * Nombre: Division
+	 * Comentario: Division por restas sucesivas
+	 * Cabecera: void Division (int numero1, int numero2, ref int cociente, ref int resto)
+	 * Precondiciones: Ambos numeros deben ser mayores que 0
+	 * Entrada: - int numero1 (Dividendo)
+	 * 			- int numero2 (Divisor)
+	 * Salida:  - int cociente
+	 * 			- int resto
+	 * E/S: No hay
+	 * Postcondiciones: Por referencia, el resto y el cociente 
+	 * 
+	 * Interfaz Java
 	 * Nombre: Division
 	 * Comentario: Division por restas sucesivas
 	 * Cabecera: void Division (int numero1, int numero2)
 	 * Precondiciones: Ambos numeros deben ser mayores que 0
-	 * Entrada: Dos enteros
-	 * Salida: Nada
+	 * Entrada: - int numero1 (Dividendo)
+	 * 			- int numero2 (Divisor)
+	 * Salida: No hay
 	 * E/S: No hay
-	 * Postcondiciones: Por referencia porque muestra tanto el resultado de la division como su resto
+	 * Postcondiciones: Pintamos tanto el cociente como el resto 
 	 * 
 	*/
 	
---
+++ EjerciciosClases/SUBPROGRAMAS/DIVIENDOYMULTIPLICANDO/FuncionalidadesRecDM.java
@@ -0,0 +1,75 @@
+/*
+ * Funcionalidades.java
+ *
+ * 
+ * 
+ */
+
+
+public class FuncionalidadesRecDM {
+	
+	/*
+	 * Nec: Dos numeros //Dividendo y divisor, Por valor
+	 * Dev: 1 numero //Cociente, Asociado al nombre
+	 * Nec/Dev: No hay
+	 * Requisitos: Ambos numeros deben ser mayores que 0
+	 * 
+	 * Interfaz
+	 * Nombre: DivisionRec
+	 * Comentario: Division por restas sucesivas
+	 * Cabecera: int DivisionRec (int numero1, int numero2)
+	 * Precondiciones: Ambos numeros deben ser mayores que 0
+	 * Entrada: - int numero1 (Dividendo)
+	 * 			- int numero2 (Divisor)
+	 * 
+	 * Salida: - int res (Cociente)
+	 * E/S: No hay
+	 * Postcondiciones: Asociado al nombre. El cociente de la division 
+	 * 
+	*/
+	
+	public static int DivisionRec (int numero1, int numero2) {
+		int res = 0;
+		
+		if(numero2 > numero1){
+            
+        } 
+        else {
+			res = 1 + DivisionRec(numero1 - numero2, numero2);
+        }
+        return res;
+    }
+ 
+	/*
+	 * Nec: Dos numeros //multiplicando y multiplicador, Por valor
+	 * Dev: Un numero //Resultado de la multiplicacion, Asociado al nombre
+	 * Nec/Dev: No hay
+	 * Requisitos: Ambos numeros deben ser mayores que 0
+	 * 
+	 * Interfaz
+	 * Nombre: MultiplicacionRec
+	 * Comentario: Multiplicacion por sumas sucesivas
+	 * Cabecera: int MultiplicacionRec (int numero1, int numero2)
+	 * Precondiciones: Ambos numeros deben ser mayores que 0
+	 * Entrada: - int numero1 (multiplicando)
+	 * 			- int numero2 (multiplicador)
+	 * 
+	 * Salida: - int res (Resultado de la multiplicacion)
+	 * E/S: No hay
+	 * Postcondiciones: El resulado de la multiplicacion. Asociado al nombre
+	 * 
+	*/
+	
+	public static int MultiplicacionRec (int numero1, int numero2) {
+		int res = 0;
+		
+		if(numero2 == 0) {
+	    }
+        else {
+          res = (numero1 + MultiplicacionRec(numero1 , numero2 - 1));   
+		}
+		
+		return res;
+	}
+}
+
