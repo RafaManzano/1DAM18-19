39b7dc1b7e787968668051d2d82b2b8b8f7cb51a
RafaManzano <rafamanzanomedina@gmail.com>
2018-12-03 10:57:32 +0100

    Subiendo

---
+++ EjerciciosClases/SUBPROGRAMAS/DIVIENDOYMULTIPLICANDOMEJORADO/conductorDM.java
@@ -0,0 +1,22 @@
+/*
+ * conductorDM
+ * 
+ * 
+ */
+
+
+public class conductorDM {
+	
+	public static void main (String[] args) {
+		
+		FuncionalidadesDM.Division(12,4);
+		FuncionalidadesDM.Division(25,5);
+		FuncionalidadesDM.Division(14,3);
+		FuncionalidadesDM.Division(18,4);
+		
+		System.out.println(FuncionalidadesDM.Multiplicacion(2,6));
+		System.out.println(FuncionalidadesDM.Multiplicacion(4,3));
+		System.out.println(FuncionalidadesDM.Multiplicacion(15,4));
+	}
+}
+
